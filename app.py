36174e45e3d546fb8647d41b420bc94e5da72016
Akshay Kumar <akshaykumar954012@gmail.com>
2026-04-04 22:37:23 +0530

    added master db a hash_password() func

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask
+import hashlib
+import sqlite3
 
 app = Flask(__name__)
+app.secret_key = "sharehub_secret_key_2026"
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+USERS_ROOT = "user_directories"
+MASTER_DB = "sharehub_master.db"
 
-if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+# ─── MASTER DB SETUP ─────────────────────────────────────────────────────────
+
+def init_master_db():
+    with sqlite3.connect(USERS_ROOT) as conn:
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS users(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT UNIQUE NOT NULL,
+                password TEXT NOT NULL,
+                cretaed_at TEXT NOT NULL
+            )
+        """)
+
+        conn.commit()
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+
