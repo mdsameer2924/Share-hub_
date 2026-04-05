ed2532b59cefc390c734efa0e6d2b467e027366a
mdsameer2924 <mdsameer2681@gmail.com>
2026-04-05 10:52:21 +0530

    Upload file function added

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
+
 
 app = Flask(__name__)
-app.config['']
+app.config['Upload_folder']='uploads'
+os.makedirs(app.config['Upload_folder'],exist_ok=True)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route('/upload', methods=['POST'])
+def upload_File():
+    file=request.files['file']
+    file_path=os.path.join(app.config['Upload_folder'],file.filename)
+    file.save(file_path)
+    return f"file {file_path} sucessfully uploaded!"
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
